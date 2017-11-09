5322116e90c31fc367175b42dade92049f9606c7
NatalyCortez <flyingchair_pe12@hotmail.com>
2017-11-09 12:55:17 -0500

    subiendo archivos

---
+++ js/app.js
@@ -0,0 +1,34 @@
+do{
+  var number = prompt('Ingrese el número de su tarjeta :');// number=45588855(string)
+  if (number === null){
+    alert("Hasta luego,regrese pronto!");
+    break;
+  } else if (typeof(number) === typeof("a")){
+    var validation = isValidCard(number);
+    alert("El nùmero de tarjeta es :" + validation);
+  } else {
+
+  }
+}while(!number);
+
+
+
+function isValidCard(number){
+  var arrayNumber = [];
+  var newArray ;
+  for (var i = 0; i < number.length; i++) {
+  arrayNumber[i] = parseInt(number[i]);
+  }
+  newArray = arrayNumber.reverse();
+
+  for(var a = 1; a < newArray.length; i++){
+    if(a % 2 ===0){
+      var duplicate = newArray[a]*2;
+      if(duplicate >= 10){
+
+      }
+
+    }
+  }
+  /*return newArray;*/
+}
