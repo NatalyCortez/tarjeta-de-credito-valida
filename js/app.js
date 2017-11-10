bdbdd3b8f359b1a8d37ad22d83fdf7cad0b5620d
NatalyCortez <flyingchair_pe12@hotmail.com>
2017-11-09 23:17:59 -0500

    subiendo cambios

--- js/app.js
+++ js/app.js
@@ -1,34 +1,49 @@
-do{
+do {
   var number = prompt('Ingrese el número de su tarjeta :');// number=45588855(string)
-  if (number === null){
-    alert("Hasta luego,regrese pronto!");
+  if (number === null) {
+    alert('Hasta luego,regrese pronto!');
     break;
-  } else if (typeof(number) === typeof("a")){
+  } else if (number !== '') {
     var validation = isValidCard(number);
-    alert("El nùmero de tarjeta es :" + validation);
+    alert('El nùmero de tarjeta es :' + validation);
   } else {
 
   }
-}while(!number);
+} while (!number);
 
 
-
-function isValidCard(number){
+function isValidCard(number) {
   var arrayNumber = [];
   var newArray ;
   for (var i = 0; i < number.length; i++) {
-  arrayNumber[i] = parseInt(number[i]);
+    arrayNumber[i] = number[i];
   }
   newArray = arrayNumber.reverse();
-
-  for(var a = 1; a < newArray.length; i++){
-    if(a % 2 ===0){
-      var duplicate = newArray[a]*2;
-      if(duplicate >= 10){
-
+  var sum = 0 ;
+  var numberSum = 0 ;
+  //  empezamos a recorrer el array posicion por posicion
+  for (var j = 1; j < newArray.length; j++) {
+    //  si el indice de la posicion es par y distinto de cero se hara lo siguiente
+    if (j % 2 === 0 && j !== 0) {//  "numero"%2
+      //  duplicaremos el numero que esta en la posicion par por dos
+      var duplicate = newArray[j] * 2;//  10 tipo number
+      //  si el doble del numero es mayor o igual a diez
+      if (duplicate >= 10) {
+        //  declaro una variable que almacene el valor duplicado y lo convierta a string
+        var newDuplicate = duplicate.toString();
+        //  declaro un array vacio para guardar las cifras del numero duplicado
+        var cipherSum = [];
+        //  recorro el numero duplicado de tipo string
+        for (var x = 0;x < newDuplicate.length;x++) {
+          //  convierto las cifras del numero de tipo string a number y agrego al array sum cada cifra
+          cipherSum[x] = parseInt(newDuplicate[x]);
+        }
+        cipherSum = cipherSum[0] + cipherSum[1];
       }
-
+      sum += cipherSum;
+    } else {
+      numberSum += parseInt(newArray[j]);
     }
   }
-  /*return newArray;*/
+  var totalSum = (sum + numberSum) ;
 }
